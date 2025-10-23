62b17f530000008dd221dc37fba1411e065ed02c
daiihaii <23662061@kthcm.edu.vn>
2025-10-23 03:30:08 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,12 @@ import Layout from "./Layout";
 import Trang1 from "./Trang1";
 // @ts-ignore
 import Chitietsanpham from "./Chitietsanpham";
+//@ts-ignore
+import Trang2 from "./Trang2";
+//@ts-ignore
+import Listsanpham from "./Listsanpham";
+//@ts-ignore
+import Listproduct from "./Listproduct"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -18,6 +24,9 @@ const App = () => {
           <Route index element={<Home />} />
           <Route path="trang1" element={<Trang1 />} />
           <Route path="sanpham/:id" element={<Chitietsanpham />} />
+          <Route path="trang2" element={<Trang2 />} />
+          <Route path="Listsanpham" element={<Listsanpham />} />
+          <Route path="Listproduct" element={<Listproduct />} />
         </Route>
       </Routes>
     </BrowserRouter>
